ba980193fa2f30a563e6d0f957f01033a27d5842
Kamiechristine <christinemaina5141@gmail.com>
2024-11-23 01:33:27 +0300

    complete lab

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const height = 74;
 const message = `${name} is ${height} inches tall`;
 
 module.exports = {name, height, message};
+
+console.log(name)
+console.log(height)
+console.log(message)
--- test/indexTest.js
+++ test/indexTest.js
@@ -4,13 +4,13 @@ const { name, height, message } = require("../index.js");
 
 describe("what-is-a-test", () => {
   describe("Name", () => {
-    it('returns "Susan"', () => {
+    it('returns "Susan": () => {
       expect(name).toEqual("Susan");
     });
   });
 
   describe("Height", () => {
-    it("is less than 40 and greater than 0", () => {
+    it(is less than 40 and greater than 0: () => {
       expect(height).toBeMoreThan(0)
       expect(height).toBeLessThan(40);
     });
